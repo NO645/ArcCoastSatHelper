2cdc5cb6399fbcb9918f85f4dd4062127b0386fa
NO645 <53620309+NO645@users.noreply.github.com>
2023-03-10 08:21:56 -0600

    Add files via upload

---
+++ runCCts.bat
@@ -0,0 +1,4 @@
+set PYTHONHOME=
+set PYTHONPATH=
+cmd /C "C:\Users\RDCHLNRO\AppData\Local\Continuum\anaconda3\Scripts\activate.bat C:\Users\RDCHLNRO\AppData\Local\Continuum\anaconda3 && call activate csv2_01052023_2 && C:\Users\RDCHLNRO\AppData\Local\Continuum\anaconda3\envs\csv2_01052023_2\python C:\Users\RDCHLNRO\Desktop\coastsatv2\cc\arc_TIMESTACK.py"
+pause
